57114c744d3dac8b708b69b43d7863971d10b42b
purport <purport@purport.link>
2022-05-29 01:59:18 +0100

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,52 @@ require 'paq' {
   'folke/tokyonight.nvim';
   's1n7ax/nvim-terminal';
   'equalsraf/neovim-gui-shim';
+  'neovim/nvim-lspconfig';
+  'tpope/vim-dispatch';
+  'radenling/vim-dispatch-neovim';
+  'hrsh7th/cmp-nvim-lsp';
+  'hrsh7th/nvim-cmp';
 }
+
+local cmp = require'cmp'
+cmp.setup({
+  sources = cmp.config.sources({ {name = 'nvim_lsp'}, }),
+})
+
+local lsp = require('lspconfig')
+local lsp_util = require('lspconfig.util')
+
+vim.diagnostic.config({virtual_text = false})
+
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+end
+
+vim.lsp.set_log_level("TRACE")
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.publishDiagnostics.codeActionsInline = true
+
+-- '--background-index','--header-insertion=iwyu',
+lsp.clangd.setup{
+  cmd = { 'clangd', '--compile-commands-dir=.build' },
+  root_dir = lsp_util.root_pattern('premake5.lua'),
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+vim.cmd([[autocmd FileType c,cpp let &makeprg='ninja -C .build']])
+vim.cmd([[autocmd BufEnter *.c compiler gcc]])
 
 -- remove whitespace at end of lines
 vim.cmd([[au BufWritePre * :%s/\s\+$//e]])
@@ -45,3 +90,4 @@ require('nvim-terminal').setup({
 
 local silent = { silent = true }
 vim.api.nvim_set_keymap('n', '<leader>t', ':lua NTGlobal["terminal"]:toggle()<cr>', silent)
+vim.api.nvim_set_keymap('t', '<esc>', '<c-\\><c-n>', silent)
