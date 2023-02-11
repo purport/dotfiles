3825fc5f6ce3f63f4624e537dddad459898652c7
purport <95120638+purport@users.noreply.github.com>
2023-02-11 06:37:00 +0000

    Update init.lua
    config

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,13 +44,21 @@ vim.api.nvim_set_keymap('n', '[q', '<cmd>cp<CR>', opts)
 vim.api.nvim_set_keymap('n', ']q', '<cmd>cn<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>ccl<CR>', opts)
 
+vim.cmd [[source ~/.local/whitebox/whitebox_v0.99.0/editor_plugins/whitebox-vim/plugin/whitebox.vim]]
+
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'gerw/vim-HiLinkTrace'
   use {
     'nathom/filetype.nvim',
     config = function()
-      require("filetype").setup({})
+      require("filetype").setup({
+        overrides = {
+          extensions = {
+            idr = "idris2"
+          }
+        }
+      })
     end
   }
   use {
@@ -385,15 +393,16 @@ require('packer').startup(function(use)
       local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
       require("null-ls").setup({
         sources = {
+          -- require("null-ls").builtins.formatting.clang_format,
+          -- require("null-ls").builtins.formatting.prettier,
+          -- require("null-ls").builtins.formatting.gofmt,
           require("null-ls").builtins.formatting.csharpier,
-          require("null-ls").builtins.formatting.clang_format,
-          require("null-ls").builtins.formatting.prettier,
-          require("null-ls").builtins.formatting.goimports_reviser.with({
-            args = {
-              "-rm-unused", "-set-alias", "-format",
-              "-output", "stdout", "$FILENAME"
-            },
-          }),
+          -- require("null-ls").builtins.formatting.goimports_reviser.with({
+          --   args = {
+          --     "-rm-unused", "-set-alias", "-format",
+          --     "-output", "stdout", "$FILENAME"
+          --   },
+          -- }),
         },
         on_attach = function(client, bufnr)
           if client.supports_method 'textDocument/formatting' then
@@ -473,6 +482,138 @@ require('packer').startup(function(use)
       vim.keymap.set('n', '<C-;><C-o>', open_output, opts)
     end
   }
+
+  use {
+    'mfussenegger/nvim-dap',
+    config = function()
+      local dap = require('dap')
+      dap.adapters.lldb = {
+        type = 'executable',
+        command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+        name = 'lldb'
+      }
+      dap.configurations.cpp = {
+        {
+          name = 'Launch',
+          type = 'lldb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = false,
+          args = {},
+
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+    end
+  }
+  use {
+    "rcarriga/nvim-dap-ui",
+    requires = {"mfussenegger/nvim-dap"},
+    config = function()
+      require("dapui").setup({
+        icons = { expanded = "", collapsed = "", current_frame = "" },
+        mappings = {
+          -- Use a table to apply multiple mappings
+          expand = { "<CR>", "<2-LeftMouse>" },
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+          toggle = "t",
+        },
+        -- Use this to override mappings for specific elements
+        element_mappings = {
+          -- Example:
+          -- stacks = {
+          --   open = "<CR>",
+          --   expand = "o",
+          -- }
+        },
+        -- Expand lines larger than the window
+        -- Requires >= 0.7
+        expand_lines = vim.fn.has("nvim-0.7") == 1,
+        -- Layouts define sections of the screen to place windows.
+        -- The position can be "left", "right", "top" or "bottom".
+        -- The size specifies the height/width depending on position. It can be an Int
+        -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+        -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+        -- Elements are the elements shown in the layout (in order).
+        -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+        layouts = {
+          {
+            elements = {
+              -- Elements can be strings or table with id and size keys.
+              { id = "scopes", size = 0.25 },
+              "breakpoints",
+              "stacks",
+              "watches",
+            },
+            size = 40, -- 40 columns
+            position = "left",
+          },
+          {
+            elements = {
+              "repl",
+              "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+          },
+        },
+        controls = {
+          -- Requires Neovim nightly (or 0.8 when released)
+          enabled = true,
+          -- Display controls in this element
+          element = "repl",
+          icons = {
+            pause = "",
+            play = "",
+            step_into = "",
+            step_over = "",
+            step_out = "",
+            step_back = "",
+            run_last = "",
+            terminate = "",
+          },
+        },
+        floating = {
+          max_height = nil, -- These can be integers or a float between 0 and 1.
+          max_width = nil, -- Floats will be treated as percentage of your screen.
+          border = "single", -- Border style. Can be "single", "double" or "rounded"
+          mappings = {
+            close = { "q", "<Esc>" },
+          },
+        },
+        windows = { indent = 1 },
+        render = {
+          max_type_length = nil, -- Can be integer or nil.
+          max_value_lines = 100, -- Can be integer or nil.
+        }
+      })
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  }
+  use 'MunifTanjim/nui.nvim'
+  -- use {
+  --   "ShinKage/idris2-nvim",
+  --   requires = {'neovim/nvim-lspconfig', 'MunifTanjim/nui.nvim'},
+  --   config = function()
+  --     require('idris2').setup {}
+  --   end
+  -- }
 
   if packer_bootstrap then
     require('packer').sync()
