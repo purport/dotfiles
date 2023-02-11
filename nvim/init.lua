a97ad45a483154a83dc3d12aefa6b5b9dbd9f859
purport <95120638+purport@users.noreply.github.com>
2023-02-11 07:50:08 +0000

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,11 +44,13 @@ vim.api.nvim_set_keymap('n', '[q', '<cmd>cp<CR>', opts)
 vim.api.nvim_set_keymap('n', ']q', '<cmd>cn<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>ccl<CR>', opts)
 
-vim.cmd [[source ~/.local/whitebox/whitebox_v0.99.0/editor_plugins/whitebox-vim/plugin/whitebox.vim]]
+-- vim.cmd [[source ~/.local/whitebox/whitebox_v0.99.0/editor_plugins/whitebox-vim/plugin/whitebox.vim]]
 
+require("impatient")
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use 'gerw/vim-HiLinkTrace'
+  use 'dstein64/vim-startuptime'
+  use 'lewis6991/impatient.nvim'
   use {
     'nathom/filetype.nvim',
     config = function()
@@ -63,6 +65,7 @@ require('packer').startup(function(use)
   }
   use {
     'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
     config = function()
       local theme = require('theme').setup()
 
@@ -82,17 +85,12 @@ require('packer').startup(function(use)
       }
     end
   }
-  -- latex
   use {
     'ntessore/unicode-math.vim',
     config = function()
       -- vim.o.keymap = 'unicode-math'
     end
   }
-  -- programming language things
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-treesitter/playground'
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
@@ -121,7 +119,7 @@ require('packer').startup(function(use)
           enable = true,
         },
         playground = {
-          enable = true,
+          enable = false,
           disable = {},
           updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
           persist_queries = false, -- Whether the query persists across vim sessions
@@ -142,7 +140,6 @@ require('packer').startup(function(use)
     end
   }
   use 'tpope/vim-fugitive'
-  use 'tpope/vim-dispatch'
   use {
     'radenling/vim-dispatch-neovim',
     requires = { 'tpope/vim-dispatch' }
@@ -154,12 +151,12 @@ require('packer').startup(function(use)
     end
   }
 
-  -- selectors
-  use { 'kyazdani42/nvim-web-devicons' }
-  use { 'nvim-telescope/telescope-file-browser.nvim' }
   use {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter', 'nvim-telescope/telescope-file-browser.nvim' },
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-file-browser.nvim'
+    },
     config = function()
       local actions = require('telescope.actions')
       local telescope = require('telescope')
@@ -199,19 +196,20 @@ require('packer').startup(function(use)
       require('mini.surround').setup({})
     end
   }
-  -- lsp completion
   use {
     'stevearc/dressing.nvim',
     config = function()
       require('dressing').setup {}
     end
   }
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
   use {
     'hrsh7th/nvim-cmp',
-    requires = {'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp'},
+    requires = {
+      'stevearc/dressing.nvim',
+      'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
+      { 'hrsh7th/cmp-nvim-lsp-signature-help', after='nvim-cmp'},
+    },
     config = function()
       vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
         vim.lsp.handlers.hover, {
@@ -278,19 +276,6 @@ require('packer').startup(function(use)
             vim.lsp.protocol.make_client_capabilities()
           ),
         })
-
-      -- local format_doc = function(client, bufnr, g)
-      --   if client.server_capabilities.documentFormattingProvider then
-      --     local au_lsp = vim.api.nvim_create_augroup(g, { clear = true })
-      --     vim.api.nvim_create_autocmd("BufWritePre", {
-      --       pattern = "*",
-      --       callback = function()
-      --         vim.lsp.buf.format()
-      --       end,
-      --       group = au_lsp,
-      --     })
-      --   end
-      -- end
 
       lspconfig.tsserver.setup {
         on_attach = function(client, bufnr)
@@ -389,13 +374,18 @@ require('packer').startup(function(use)
   use {
     'jose-elias-alvarez/null-ls.nvim',
     requires = { "nvim-lua/plenary.nvim" },
+    ft = {
+      "cpp", "c", "markdown.mdx", "handlebars", "markdown", "typescript", "html",
+      "css", "scss", "graphql", "json", "less", "jsonc", "javascriptreact", "javascript",
+      "yaml", "vue", "typescriptreact", "go", "cs"
+    },
     config = function()
       local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
       require("null-ls").setup({
         sources = {
-          -- require("null-ls").builtins.formatting.clang_format,
-          -- require("null-ls").builtins.formatting.prettier,
-          -- require("null-ls").builtins.formatting.gofmt,
+          require("null-ls").builtins.formatting.clang_format.with({filetypes = {"c", "cpp"}}),
+          require("null-ls").builtins.formatting.prettier,
+          require("null-ls").builtins.formatting.gofmt,
           require("null-ls").builtins.formatting.csharpier,
           -- require("null-ls").builtins.formatting.goimports_reviser.with({
           --   args = {
@@ -419,10 +409,9 @@ require('packer').startup(function(use)
       })
     end
   }
-  -- zettlekasten
   use {
     "mickael-menu/zk-nvim",
-    requires = { 'telescope.nvim', 'hrsh7th/nvim-cmp' },
+    requires = { 'telescope.nvim' },
     config = function()
       local zk = require("zk")
       zk.setup({
@@ -450,163 +439,163 @@ require('packer').startup(function(use)
       vim.api.nvim_set_keymap("v", "<leader>zb", ":ZkBacklinks", opts)
     end
   }
-  use 'Issafalcon/neotest-dotnet'
-  use {
-    "nvim-neotest/neotest",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
-      'Issafalcon/neotest-dotnet'
-    },
-    config = function()
-      local lib = require("neotest.lib")
-      local DotnetNeotestAdapter = require("neotest-dotnet")
-      DotnetNeotestAdapter.root = lib.files.match_root_pattern("*.sln")
-
-      require("neotest").setup({
-        adapters = {
-          DotnetNeotestAdapter
-        },
-      })
-      local function run_test_file() require("neotest").run.run(vim.fn.expand('%')) end
-      local function run_test_suite() require("neotest").run.run({vim.fn.expand('%'), suite=true}) end
-      local function toggle_summary() require("neotest").summary.toggle() end
-      local function open_output() require("neotest").output.open() end
-
-      local opts = { silent = true, noremap = true }
-      -- vim.keymap.set('n', '<C-;><C-r>', run_test_file, opts)
-      vim.keymap.set('n', '<C-;><C-r>', run_test_file, opts)
-      vim.keymap.set('n', '<C-;><C-t>', run_test_suite, opts)
-      vim.keymap.set('n', '<C-;><C-s>', toggle_summary, opts)
-      vim.keymap.set('n', '<C-;><C-o>', open_output, opts)
-    end
-  }
-
-  use {
-    'mfussenegger/nvim-dap',
-    config = function()
-      local dap = require('dap')
-      dap.adapters.lldb = {
-        type = 'executable',
-        command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
-        name = 'lldb'
-      }
-      dap.configurations.cpp = {
-        {
-          name = 'Launch',
-          type = 'lldb',
-          request = 'launch',
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          cwd = '${workspaceFolder}',
-          stopOnEntry = false,
-          args = {},
-
-        },
-      }
-      dap.configurations.c = dap.configurations.cpp
-      dap.configurations.rust = dap.configurations.cpp
-    end
-  }
-  use {
-    "rcarriga/nvim-dap-ui",
-    requires = {"mfussenegger/nvim-dap"},
-    config = function()
-      require("dapui").setup({
-        icons = { expanded = "", collapsed = "", current_frame = "" },
-        mappings = {
-          -- Use a table to apply multiple mappings
-          expand = { "<CR>", "<2-LeftMouse>" },
-          open = "o",
-          remove = "d",
-          edit = "e",
-          repl = "r",
-          toggle = "t",
-        },
-        -- Use this to override mappings for specific elements
-        element_mappings = {
-          -- Example:
-          -- stacks = {
-          --   open = "<CR>",
-          --   expand = "o",
-          -- }
-        },
-        -- Expand lines larger than the window
-        -- Requires >= 0.7
-        expand_lines = vim.fn.has("nvim-0.7") == 1,
-        -- Layouts define sections of the screen to place windows.
-        -- The position can be "left", "right", "top" or "bottom".
-        -- The size specifies the height/width depending on position. It can be an Int
-        -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-        -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-        -- Elements are the elements shown in the layout (in order).
-        -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-        layouts = {
-          {
-            elements = {
-              -- Elements can be strings or table with id and size keys.
-              { id = "scopes", size = 0.25 },
-              "breakpoints",
-              "stacks",
-              "watches",
-            },
-            size = 40, -- 40 columns
-            position = "left",
-          },
-          {
-            elements = {
-              "repl",
-              "console",
-            },
-            size = 0.25, -- 25% of total lines
-            position = "bottom",
-          },
-        },
-        controls = {
-          -- Requires Neovim nightly (or 0.8 when released)
-          enabled = true,
-          -- Display controls in this element
-          element = "repl",
-          icons = {
-            pause = "",
-            play = "",
-            step_into = "",
-            step_over = "",
-            step_out = "",
-            step_back = "",
-            run_last = "",
-            terminate = "",
-          },
-        },
-        floating = {
-          max_height = nil, -- These can be integers or a float between 0 and 1.
-          max_width = nil, -- Floats will be treated as percentage of your screen.
-          border = "single", -- Border style. Can be "single", "double" or "rounded"
-          mappings = {
-            close = { "q", "<Esc>" },
-          },
-        },
-        windows = { indent = 1 },
-        render = {
-          max_type_length = nil, -- Can be integer or nil.
-          max_value_lines = 100, -- Can be integer or nil.
-        }
-      })
-      local dap, dapui = require("dap"), require("dapui")
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end
-  }
-  use 'MunifTanjim/nui.nvim'
+  -- use 'Issafalcon/neotest-dotnet'
+  -- use {
+  --   "nvim-neotest/neotest",
+  --   requires = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "antoinemadec/FixCursorHold.nvim",
+  --     'Issafalcon/neotest-dotnet'
+  --   },
+  --   config = function()
+  --     local lib = require("neotest.lib")
+  --     local DotnetNeotestAdapter = require("neotest-dotnet")
+  --     DotnetNeotestAdapter.root = lib.files.match_root_pattern("*.sln")
+  --
+  --     require("neotest").setup({
+  --       adapters = {
+  --         DotnetNeotestAdapter
+  --       },
+  --     })
+  --     local function run_test_file() require("neotest").run.run(vim.fn.expand('%')) end
+  --     local function run_test_suite() require("neotest").run.run({vim.fn.expand('%'), suite=true}) end
+  --     local function toggle_summary() require("neotest").summary.toggle() end
+  --     local function open_output() require("neotest").output.open() end
+  --
+  --     local opts = { silent = true, noremap = true }
+  --     -- vim.keymap.set('n', '<C-;><C-r>', run_test_file, opts)
+  --     vim.keymap.set('n', '<C-;><C-r>', run_test_file, opts)
+  --     vim.keymap.set('n', '<C-;><C-t>', run_test_suite, opts)
+  --     vim.keymap.set('n', '<C-;><C-s>', toggle_summary, opts)
+  --     vim.keymap.set('n', '<C-;><C-o>', open_output, opts)
+  --   end
+  -- }
+  --
+  -- use {
+  --   'mfussenegger/nvim-dap',
+  --   config = function()
+  --     local dap = require('dap')
+  --     dap.adapters.lldb = {
+  --       type = 'executable',
+  --       command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+  --       name = 'lldb'
+  --     }
+  --     dap.configurations.cpp = {
+  --       {
+  --         name = 'Launch',
+  --         type = 'lldb',
+  --         request = 'launch',
+  --         program = function()
+  --           return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  --         end,
+  --         cwd = '${workspaceFolder}',
+  --         stopOnEntry = false,
+  --         args = {},
+  --
+  --       },
+  --     }
+  --     dap.configurations.c = dap.configurations.cpp
+  --     dap.configurations.rust = dap.configurations.cpp
+  --   end
+  -- }
+  -- use {
+  --   "rcarriga/nvim-dap-ui",
+  --   requires = {"mfussenegger/nvim-dap"},
+  --   config = function()
+  --     require("dapui").setup({
+  --       icons = { expanded = "", collapsed = "", current_frame = "" },
+  --       mappings = {
+  --         -- Use a table to apply multiple mappings
+  --         expand = { "<CR>", "<2-LeftMouse>" },
+  --         open = "o",
+  --         remove = "d",
+  --         edit = "e",
+  --         repl = "r",
+  --         toggle = "t",
+  --       },
+  --       -- Use this to override mappings for specific elements
+  --       element_mappings = {
+  --         -- Example:
+  --         -- stacks = {
+  --         --   open = "<CR>",
+  --         --   expand = "o",
+  --         -- }
+  --       },
+  --       -- Expand lines larger than the window
+  --       -- Requires >= 0.7
+  --       expand_lines = vim.fn.has("nvim-0.7") == 1,
+  --       -- Layouts define sections of the screen to place windows.
+  --       -- The position can be "left", "right", "top" or "bottom".
+  --       -- The size specifies the height/width depending on position. It can be an Int
+  --       -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  --       -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  --       -- Elements are the elements shown in the layout (in order).
+  --       -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  --       layouts = {
+  --         {
+  --           elements = {
+  --             -- Elements can be strings or table with id and size keys.
+  --             { id = "scopes", size = 0.25 },
+  --             "breakpoints",
+  --             "stacks",
+  --             "watches",
+  --           },
+  --           size = 40, -- 40 columns
+  --           position = "left",
+  --         },
+  --         {
+  --           elements = {
+  --             "repl",
+  --             "console",
+  --           },
+  --           size = 0.25, -- 25% of total lines
+  --           position = "bottom",
+  --         },
+  --       },
+  --       controls = {
+  --         -- Requires Neovim nightly (or 0.8 when released)
+  --         enabled = true,
+  --         -- Display controls in this element
+  --         element = "repl",
+  --         icons = {
+  --           pause = "",
+  --           play = "",
+  --           step_into = "",
+  --           step_over = "",
+  --           step_out = "",
+  --           step_back = "",
+  --           run_last = "",
+  --           terminate = "",
+  --         },
+  --       },
+  --       floating = {
+  --         max_height = nil, -- These can be integers or a float between 0 and 1.
+  --         max_width = nil, -- Floats will be treated as percentage of your screen.
+  --         border = "single", -- Border style. Can be "single", "double" or "rounded"
+  --         mappings = {
+  --           close = { "q", "<Esc>" },
+  --         },
+  --       },
+  --       windows = { indent = 1 },
+  --       render = {
+  --         max_type_length = nil, -- Can be integer or nil.
+  --         max_value_lines = 100, -- Can be integer or nil.
+  --       }
+  --     })
+  --     local dap, dapui = require("dap"), require("dapui")
+  --     dap.listeners.after.event_initialized["dapui_config"] = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.event_terminated["dapui_config"] = function()
+  --       dapui.close()
+  --     end
+  --     dap.listeners.before.event_exited["dapui_config"] = function()
+  --       dapui.close()
+  --     end
+  --   end
+  -- }
+  -- use 'MunifTanjim/nui.nvim'
   -- use {
   --   "ShinKage/idris2-nvim",
   --   requires = {'neovim/nvim-lspconfig', 'MunifTanjim/nui.nvim'},
